90f13aef327b41cc82b2bef7def2d92dcc147e25
huenbin <huenbin@foxmail.com>
2018-06-13 17:55:45 +0800

    提交

--- src/main/java/com/enhinck/demo/config/HttpsConfig.java
+++ src/main/java/com/enhinck/demo/config/HttpsConfig.java
@@ -1,11 +1,17 @@
 package com.enhinck.demo.config;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 /**
@@ -30,7 +36,7 @@ public class HttpsConfig {
 				context.addConstraint(constraint);
 			}
 		};
-		//tomcat.addAdditionalTomcatConnectors(httpConnector());
+		tomcat.addAdditionalTomcatConnectors(createSslConnector());
 		return tomcat;
 	}
 
@@ -49,39 +55,27 @@ public class HttpsConfig {
 
 	
 	
-	/*private Connector createSslConnector() {
+	private Connector createSslConnector() {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
 		try {
-			File keystore = new ClassPathResource("cas-enhinck.keystore").getFile();
-			File truststore = new ClassPathResource("ENHINCK.cer").getFile();
+			File keystore = new ClassPathResource("springboothttps.keystore").getFile();
+			File truststore = new ClassPathResource("springboothttps.keystore").getFile();
 			connector.setScheme("https");
 			connector.setSecure(true);
 			connector.setPort(8443);
 			protocol.setSSLEnabled(true);
 			protocol.setKeystoreFile(keystore.getAbsolutePath());
-			protocol.setKeystorePass("enhinck");
+			protocol.setKeystorePass("123456");
 			protocol.setTruststoreFile(truststore.getAbsolutePath());
-			protocol.setTruststorePass("changeit");
-			protocol.setKeyAlias("ENHINCK");
+			protocol.setTruststorePass("123456");
+			protocol.setKeyAlias("springboothttps");
 			return connector;
 		}
 		catch (IOException ex) {
 			throw new IllegalStateException("can't access keystore: [" + "keystore"
 					+ "] or truststore: [" + "keystore" + "]", ex);
 		}
-	}*/
+	}
 	
-	
-	/*@Bean
-	public Connector httpConnector() {
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setScheme("http");
-		// Connector监听的http的端口号
-		connector.setPort(8081);
-		connector.setSecure(false);
-		// 监听到http的端口号后转向到的https的端口号
-		connector.setRedirectPort(8443);
-		return connector;
-	}*/
 }
